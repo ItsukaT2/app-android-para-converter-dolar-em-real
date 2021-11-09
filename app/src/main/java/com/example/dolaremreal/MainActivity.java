6d5e96b28e48b19410e786bb3add33ed80ba3570
itsukat2 <kenzomatsuta@gmail.com>
2021-11-09 15:34:50 -0300

    thirt commit

--- app/src/main/java/com/example/dolaremreal/MainActivity.java
+++ app/src/main/java/com/example/dolaremreal/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.dolaremreal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     EditText txtprecodolar, txtvalor;
     TextView txtresultado;
+    TextView valdol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +28,28 @@ public class MainActivity extends AppCompatActivity {
 
         txtresultado = findViewById(R.id.txtresultado);
 
+        valdol = (TextView) findViewById(R.id.valdol);
+        valdol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=valor+do+dolar&oq=valor+do&aqs=chrome.1.69i57j0i433i512l3j0i433i457i512j0i402j69i61j69i60.2440j0j7&sourceid=chrome&ie=UTF-8")));
+            }
+        });
+
     }
 
+    @SuppressLint("SetTextI18n")
     public void calcular(View view) {
 
         double valor = Double.parseDouble(txtvalor.getText().toString());
         double preco = Double.parseDouble(txtprecodolar.getText().toString());
 
         double real = preco * valor;
+
         DecimalFormat decimal = new DecimalFormat("0.00");
         String valorformatado = decimal.format(real);
 
-        txtresultado.setText("R$: " + valorformatado);
+        txtresultado.setText(valorformatado + "R$");
 
     }
 
